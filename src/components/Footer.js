e3696dd086b88001bccfa586e4a02d70c6df5e76
limyoursun <limyoursun@gmail.com>
2024-03-28 20:56:05 +0900

    setting_bak

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () =>{
+    return(
+        <footer>저는 Footer 입니다</footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import './../styles/Header.module.scss'
+
+const Header = () => {
+    return(
+        <header>저는 Header 입니다</header>
+    )
+}
+
+export default Header
