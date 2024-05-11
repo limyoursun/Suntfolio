2667e49c1d6eb838fa2c30cf7395b68b0eeafa65
limyoursun <limyoursun@gmail.com>
2024-05-11 19:03:42 +0900

    bak

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,32 @@
 /* css import */
-import style from "../styles/Footer.module.scss"
+import style from "../styles/Footer.module.scss";
 
-const Footer = () =>{
-    return(
-        <footer>
-            <small className={style.copy}>&copy; 2024. LIMSUN. All rights reserved.</small>
-        </footer>
-    )
-}
-export default Footer
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <p>Test</p>
+        <table>
+          <caption>page info</caption>
+          <thead>
+            <tr>
+              <th>PERIOD</th>
+              <th>type of work</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>202103 202310</td>
+              <td>PUBLISHING</td>
+            </tr>
+            <tr>
+              <td colSpan={2}>&copy; 2024. LIMSUN. All rights reserved.</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <p>Test</p>
+    </footer>
+  );
+};
+export default Footer;
