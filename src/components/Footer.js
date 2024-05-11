d6acf664e3369ccb764046944d29c82425763411
limyoursun <limyoursun@gmail.com>
2024-05-11 21:35:07 +0900

    bak

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,25 @@
+import { Link } from "react-router-dom";
+
 /* css import */
 import style from "../styles/Footer.module.scss";
 
+const url = "https://github.com/limyoursun"
 const Footer = () => {
   return (
     <footer>
-      <div>
-        <p>Test</p>
-        <table>
-          <caption>page info</caption>
-          <thead>
-            <tr>
-              <th>PERIOD</th>
-              <th>type of work</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>202103 202310</td>
-              <td>PUBLISHING</td>
-            </tr>
-            <tr>
-              <td colSpan={2}>&copy; 2024. LIMSUN. All rights reserved.</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <p>Test</p>
+      <table>
+        <caption>page info</caption>
+        <tbody>
+          <tr>
+            <td rowSpan={2}><span>S</span></td>
+            <td>PUBLISHING PORTFOLIO</td>
+          </tr>
+          <tr>
+            <td><small>&copy;</small> 2024. LIMSUN. All rights reserved.</td>
+          </tr>
+        </tbody>
+      </table>
+      <button onClick={() => {window.open(url)}}>GITHUB</button>
     </footer>
   );
 };
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -4,8 +4,12 @@ import { Link } from "react-router-dom";
 import style from "../styles/Home.module.scss";
 
 /* icon import */
-import { ArrowRight, PersonBoundingBox, BriefcaseFill} from 'react-bootstrap-icons';
-import * as Icons from 'react-bootstrap-icons';
+import {
+  ArrowRight,
+  PersonBoundingBox,
+  BriefcaseFill,
+} from "react-bootstrap-icons";
+import * as Icons from "react-bootstrap-icons";
 
 /* component import */
 import BtnCircle from "../components/BtnCircle";
@@ -23,47 +27,48 @@ function Project({
   demo,
   test,
   theme,
-  icon
+  icon,
 }) {
   const BadgeIcon = category === "개인" ? PersonBoundingBox : BriefcaseFill;
   const IconComponent = Icons[icon];
 
   useEffect(() => {
     CardSlide();
-  })
-  
+  });
+
   return (
     <li key="project.id">
-      <Link
-        to={demo}
-        className={style.tumb}
-        style={{ backgroundColor: `${theme}` }}
-      >
-        {IconComponent && <IconComponent />}
-      </Link>
       <span>{client}</span>
-      <h2>{name}</h2>
-      <p>
-        {description.length > 100
-          ? `${description.slice(0, 100)}`
-          : description}
-      </p>
-      <div className="builtwith_wrap">
-        {test.map((g) => (
-          <span key={g}>{g}</span>
-        ))}
-      </div>
-      <div className={style.btn_wrap}>
-        <div className={style.category}>
-          <BtnCircle/>
-          <BadgeIcon/>
+      <div>
+        <h2>{name}</h2>
+        <p>
+          {description.length > 100
+            ? `${description.slice(0, 100)}`
+            : description}
+        </p>
+        <div className="builtwith_wrap">
+          {test.map((g) => (
+            <span key={g}>{g}</span>
+          ))}
         </div>
-        <Link to={`detail/${id}`} className="btn_site">
-          <ArrowRight className="icon"/>
+        <div className={style.btn_wrap}>
+          <div className={style.category}>
+            <BtnCircle />
+            <BadgeIcon />
+          </div>
+          <Link to={`detail/${id}`} className="btn_site">
+            <ArrowRight className="icon" />
+          </Link>
+        </div>
+        <Link
+          to={demo}
+          className={style.tumb}
+          style={{ backgroundColor: `${theme}` }}
+        >
+          {IconComponent && <IconComponent />}
         </Link>
       </div>
     </li>
-    
   );
 }
 
