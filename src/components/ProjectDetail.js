b8380e7dd0ea4c55f66e6b72d2ca49a9f38f422d
limyoursun <limyoursun@gmail.com>
2024-05-17 02:08:15 +0900

    bak

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -24,19 +24,19 @@ function ProjectDetail({
   return (
     <>
       <section className={style.detail_con}>
-        <aside>
-          <p>
-            이곳은 프로젝트에 대한 자세한 설명과 함께, 제가 맡은 역할과 작업한
-            범위를 구체적으로 기재합니다.
-            <br />
-            프로젝트에서 사용한 기술과 도구에 대해서도 간략하게 소개해드립니다.
-            <br />
-            프로젝트를 작업하며 신경 쓴 주요 기능과 화면을 직접 확인하실 수
-            있어요!
-          </p>
-        </aside>
         <article>
           <div className={style.con_info}>
+          <aside>
+            <p>
+              이곳은 프로젝트에 대한 자세한 설명과 함께, 제가 맡은 역할과 작업한
+              범위를 구체적으로 기재합니다.
+              <br />
+              프로젝트에서 사용한 기술과 도구에 대해서도 간략하게 소개해드립니다.
+              <br />
+              프로젝트를 작업하며 신경 쓴 주요 기능과 화면을 직접 확인하실 수
+              있어요!
+            </p>
+          </aside>
             <div>
               <h2>{name}</h2>
               <ul className="builtwith_wrap">
