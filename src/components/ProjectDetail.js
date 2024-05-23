89876add90ea33168a307385ba348b5bdc46fd33
limyoursun <limyoursun@gmail.com>
2024-05-24 01:19:42 +0900

    bak

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -22,35 +22,38 @@ function ProjectDetail({
   design,
   publishing
 }) {
+
   const IconComponent = Icons[icon];
 
   return (
     <>
       <section className={style.detail_con} >
         <div>
-          <h4 className={style.title}>Title</h4>
-          <h3 className={style.main_title}>Title</h3>
-          <p className={style.paragraph}>
-          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-          </p>
+          <h4 className={style.title}>{category}</h4>
+          <span>{period}</span>
+          <h3>{name}</h3>
+          <div className={style.description}>
+            <div className={style.img_wrap}>
+              <img src={`../images/${image}`} alt={name}/>
+            </div>
+            <p className={style.paragraph}>
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
+            </p>
+
+          </div>
         </div>
         <div className={style.con_info}>
           <span>{client}</span>
           <span>{category}</span>
-          <h3>{name}</h3>
+          
           <p>{description}</p>
           <Link to={demo}>demo</Link>
-          <ul>
-            <li>{theme}</li>
-            <li>{subTheme}</li>
-          </ul>
           <h3>{icon}</h3>
-          <span>{period}</span>
+          
           <h3>{skill}</h3>
           <p>{design}</p>
           <p>{publishing}</p>
         </div>
-        <img src={`../images/${image}`} alt={name}/>
       </section>
     </>
   );
