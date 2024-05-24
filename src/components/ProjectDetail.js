d8f71838da0c84e0965adeced248cd7408e2518b
limyoursun <limyoursun@gmail.com>
2024-05-24 19:33:07 +0900

    bak

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -20,54 +20,69 @@ function ProjectDetail({
   period,
   skill,
   design,
-  publishing
+  publishing,
 }) {
-
   const IconComponent = Icons[icon];
 
   return (
     <>
-      <section className={style.detail_con} >
+      <section className={style.detail_con}>
         <div>
           <h4 className={style.title}>{category}</h4>
           <span>{period}</span>
           <h3>{name}</h3>
-          <div className={style.description}>
-            <div className={style.img_wrap}>
-              <img src={`../images/${image}`} alt={name}/>
-            </div>
-            <div>
-              <span>{category}</span>
-              <span>{period}</span>
-              <span>{category}</span>
-            </div>
 
-          </div>
-          <div className={style.description}>
-            <p className={style.paragraph}>
-            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
-            </p>
-            <div className={style.img_wrap}>
-              <img src={`../images/${image}`} alt={name}/>
+          <div className={style.description_wrap}>
+            <div className={style.description}>
+              <div className={style.img_wrap}>
+                <img src={`../images/${image}`} alt={name} />
+              </div>
+              <div>
+                <span>{category}</span>
+                <span>{period}</span>
+                <span>{category}</span>
+              </div>
             </div>
-          </div>
-          <div className={style.description}>
-            <div className={style.img_wrap}>
-              <img src={`../images/${image}`} alt={name}/>
+            <div className={style.description}>
+              <p className={style.paragraph}>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+                The point of using Lorem Ipsum is that it has a more-or-less
+                normal distribution of letters, as opposed to using 'Content
+                here, content here', making it look like readable English. Many
+                desktop publishing packages and web page editors now use Lorem
+                Ipsum as their default model text, and a search for 'lorem
+                ipsum' will uncover many web sites still in their infancy.
+              </p>
+              <div className={style.img_wrap}>
+                <img src={`../images/${image}`} alt={name} />
+              </div>
             </div>
-            <p className={style.paragraph}>
-            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
-            </p>
+            <div className={style.description}>
+              <div className={style.img_wrap}>
+                <img src={`../images/${image}`} alt={name} />
+              </div>
+              <p className={style.paragraph}>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+                The point of using Lorem Ipsum is that it has a more-or-less
+                normal distribution of letters, as opposed to using 'Content
+                here, content here', making it look like readable English. Many
+                desktop publishing packages and web page editors now use Lorem
+                Ipsum as their default model text, and a search for 'lorem
+                ipsum' will uncover many web sites still in their infancy.
+              </p>
+            </div>
           </div>
         </div>
         <div className={style.con_info}>
           <span>{client}</span>
           <span>{category}</span>
-          
+
           <p>{description}</p>
           <Link to={demo}>demo</Link>
           <h3>{icon}</h3>
-          
+
           <h3>{skill}</h3>
           <p>{design}</p>
           <p>{publishing}</p>
