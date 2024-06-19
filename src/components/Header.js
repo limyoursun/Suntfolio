18e76c017f3b0b08edaa27aab0c04d3245a52f9b
limyoursun <limyoursun@gmail.com>
2024-06-19 20:39:40 +0900

    bak

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-    return(
-        <header>
-            <h1>LIM + SUN 임선 포트폴리오
-                <img src='../images/logo.gif' alt='임선 포트폴리오 로고'/>
-            </h1>
-        </header>
-    )
-}
-export default Header
+  return (
+    <header>
+      <Link to='/'>
+        <h1>
+          LIM + SUN 임선 포트폴리오
+          <img src="../images/logo.gif" alt="임선 포트폴리오 로고" />
+        </h1>
+      </Link>
+    </header>
+  );
+};
+export default Header;
